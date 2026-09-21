d30c698be307c843e69c386b8a092c550673dbc1
madkarmaa <madkarma99@gmail.com>
2026-09-21 16:00:54 +0200

    feat: enable extract native libs for all apps

---
+++ apps/universal/patch/src/main/kotlin/top/madkarma/patches/universal/ForceExtractNativeLibsPatch.kt
@@ -0,0 +1,28 @@
+@file:Suppress("unused")
+
+package top.madkarma.patches.universal
+
+import app.reseam.patch.patch
+
+val forceExtractNativeLibs = patch("Force extract native libs") {
+    description("Sets android:extractNativeLibs to true when it is false.")
+    enabledByDefault(true)
+
+    execute {
+        manifest.edit {
+            val application = findByTag("application").firstOrNull()
+                ?: error("ExtractNativeLibs: manifest has no <application> element")
+
+            when (application["android:extractNativeLibs"]?.lowercase()) {
+                "false" -> {
+                    application["android:extractNativeLibs"] = "true"
+                    log.info("ExtractNativeLibs: flipped to true.")
+                }
+
+                "true" -> log.info("ExtractNativeLibs: already true, skipped.")
+                null -> log.info("ExtractNativeLibs: attribute absent, skipped.")
+                else -> log.info("ExtractNativeLibs: unexpected value, skipped.")
+            }
+        }
+    }
+}
